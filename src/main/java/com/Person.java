bdaba6cbb4e887f400f12fdff96f53626dd9618c
wbadmin <>
2020-01-09 11:20:14 +0100

    {/src/main/java/com/Person.java}

--- src/main/java/com/Person.java
+++ src/main/java/com/Person.java
@@ -6,12 +6,44 @@ package com;
 
 public class Person implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Person() {
-    }
+	private java.lang.String name;
+	private java.lang.Integer age;
+	private java.lang.Boolean adult;
 
+	public Person() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(java.lang.Integer age) {
+		this.age = age;
+	}
+
+	public java.lang.Boolean getAdult() {
+		return this.adult;
+	}
+
+	public void setAdult(java.lang.Boolean adult) {
+		this.adult = adult;
+	}
+
+	public Person(java.lang.String name, java.lang.Integer age,
+			java.lang.Boolean adult) {
+		this.name = name;
+		this.age = age;
+		this.adult = adult;
+	}
 
 }
